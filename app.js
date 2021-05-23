0c445c01e002a70d6b18199c33cd9a072986044b
KAUSTAV BHATTACHARYA <kaustav.bhattacharya@codelogicx.com>
2021-05-23 20:35:14 +0530

    set up view engine and views

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-const dotenv  = require("dotenv").config() 
-const express = require("express")
-const app     = express() 
-const port    = process.env.PORT || 3000
+const dotenv  = require("dotenv").config(); 
+const express = require("express");
+const path    = require("path")
+const handlebars = require('express-handlebars');
+
+
+const app     = express(); 
+const port    = process.env.PORT || 3000;
+
+
+//Sets our app to use the handlebars engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
+
+//Sets handlebars configurations (we will go through them later on)
+app.engine('handlebars', handlebars());
+
+app.use(express.static('public'))
 
 app.get("/",function(req,res){
-     res.send("hello world")
+     res.render("index",{title:"Language Translator",layout:false})
 });
 
 app.listen(port,function(){
