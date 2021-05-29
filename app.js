96c7b4e0e7b6151c3a65c2de1815b32f5b9a8348
KAUSTAV BHATTACHARYA <kaustav.bhattacharya@codelogicx.com>
2021-05-30 00:10:37 +0530

    Primary setup.

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const dotenv  = require("dotenv").config(); 
 const express = require("express");
 const path    = require("path")
-const handlebars = require('express-handlebars');
+const cons = require('consolidate');
 
 
 const app     = express(); 
 const port    = process.env.PORT || 3000;
 
 
-//Sets our app to use the handlebars engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'handlebars');
+// //Sets our app to use the handlebars engine
+ app.set('views', path.join(__dirname, 'views'));
+ app.set('view engine', 'html');
 
-//Sets handlebars configurations (we will go through them later on)
-app.engine('handlebars', handlebars());
+// //Sets handlebars configurations (we will go through them later on)
+ app.engine('html',cons.swig);
 
-app.use(express.static('public'))
+app.use('/public',express.static('public'))
 
 app.get("/",function(req,res){
-     res.render("index",{title:"Language Translator",layout:false})
+     res.render("index")
 });
 
 app.listen(port,function(){
